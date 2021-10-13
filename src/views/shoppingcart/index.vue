0cf2aa0e675f8cf2991cb84838384644d6a44749
jackqj888 <11325176@qq.com>
2021-10-13 17:53:35 +0800

    购物车基本完成

--- src/views/shoppingcart/index.vue
+++ src/views/shoppingcart/index.vue
@@ -3,7 +3,7 @@
     <div class="cartList">
       <div class="cartListBox" v-for="(value,index) in CartAddList" :key="index">
         <div class="cartListBox_left">
-          <img src="@/assets/item.png" />
+          <img src="@/assets/item.png"/>
         </div>
         <div class="cartListBox_center">
           <div class="title">
@@ -17,17 +17,17 @@
             <p class="price">
               价格
               <span style="color: #d83041;">
-               ¥ {{ value.totalPrice }}
+               ¥ {{ value.price * value.quantity }}
               </span>
             </p>
           </div>
-          
+
         </div>
         <div class="cartListBox_right">
-          <button class="add" @click="add">增加</button>
-          <button class="reduce" @click="sub">减少</button>
-          <button class="Selected">选中</button>
-          <button class="remove">删除</button>
+          <button class="add" @click="add(index)">增加</button>
+          <button class="reduce" @click="sub(index)">减少</button>
+          <button class="Selected" :class="current === index ? 'active ' : ''" @click="isBlue(index)">选中</button>
+          <button class="remove" @click="remove">删除</button>
         </div>
       </div>
     </div>
@@ -41,79 +41,97 @@ import api from "@/api";
 export default {
   data() {
     return {
-      CartAddList:[],
-      cartNum:1,
+      CartAddList: [],
+      cartNum: 1,
       productId: '',
+      totalPrice: '',
+      isActive:false,
+      current:''
     }
   },
   created() {
     this.getCartList()
   },
+  
   methods: {
     getCartList() {
       api.cart.cartList().then(res => {
         this.CartAddList = res
-       this.productId = this.CartAddList.productId
-        this.cartNum = this.CartAddList.cartNum
         console.log('zz1z', this.CartAddList)
-
       })
     },
-    add(){
-      let data = {
-        productId: this.productId,
-        count: this.cartNum
+    add(i) {
+      this.CartAddList[i].quantity++
+      this.totalPrice = this.CartAddList[i].totalPrice
+      console.log('nnn',this.totalPrice)
+    },
+    sub(i) {
+      this.CartAddList[i].quantity--
+      if (this.CartAddList[i].quantity <= 0) {
+        this.CartAddList.splice(i, 1)
       }
-      api.cart.countUp(data).then(res=> {
-        this.cartNum = res.quantity
-        this.cartNum++
-      })
-      
     },
-    sub(){
-      this.num--
+    remove(i){
+      this.CartAddList.splice(i, 1)
+    },
+    isBlue(i){
+      this.current = i
+      this.isActive = !this.isActive
+      
+     
+      
+      
     }
+    
   }
 }
 </script>
 
 <style scoped lang="stylus">
- .container
-   margin: 20px 0
-   display: flex
-   justify-content:center
-  .cartList
-    width: 1200px
-    height: 760px
+.container
+  margin: 20px 0
+  display: flex
+  justify-content: center
+
+.cartList
+  width: 1200px
+  height: 760px
+  display: flex
+  justify-content: center
+  flex-wrap: wrap
+
+  .cartListBox
+    margin: 5px 0
+    width: 800px
+    height: 180px
+    border: 1px solid #cbcbcb
     display: flex
-    justify-content:center
-    flex-wrap:wrap
-    .cartListBox
-      margin: 5px 0
-      width: 800px
-      height: 180px
-      border: 1px solid #cbcbcb
+    justify-content: space-around
+    align-items: center
+
+    .cartListBox_center
+      width: 250px
+      margin-right: 120px;
+
+      .num
+        margin-top: 10px
+
+    .cartListBox_right
+      width: 200px
+      height: 126px
       display: flex
-      justify-content:space-around
+      flex-wrap: wrap;
+      justify-content: center
       align-items: center
-      .cartListBox_center
-        width: 250px
-        margin-right: 120px;
-        .num
-          margin-top: 10px
-      .cartListBox_right
-        width: 200px
-        height: 126px
-        display: flex
-        flex-wrap: wrap;
-        justify-content:center
-        align-items: center
-        .add,.reduce,.Selected,.remove
-          flex:1
-          min-width:30%
-          max-width:30%
-          width: 50px
-          margin:0 10px
+
+      .add, .reduce, .Selected, .remove
+        flex: 1
+        min-width: 30%
+        max-width: 30%
+        width: 50px
+        margin: 0 10px
+      .active
+        color: #32bcf3
 
 
 </style>
